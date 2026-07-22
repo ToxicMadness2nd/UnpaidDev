afae80bc7562350b3d00acebb57923547784c991
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-07-22 08:42:28 +0800

    Update test.php

---
+++ test.php
@@ -0,0 +1,6 @@
+git pull origin main --allow-unrelated-histories   
+git add .
+git commit -m "Merged unrelated histories"
+git add .
+git commit -m "Merged unrelated histories"
+git push --set-upstream origin main
